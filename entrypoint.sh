e035e0a5440d074b1931728a5eaf58477e4cf1db
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-21 21:21:01 -0700

    yes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 
 echo "Starting: Push Artifacts"
 
-echo "0: ${0}"
-echo "1: ${1}"
-echo "2: ${2}"
+#echo "0: ${0}"
+#echo "1: ${1}"
+#echo "2: ${2}"
 
 echo "INPUT_PATH: ${INPUT_PATH}"
+
 ls -lAhR "${INPUT_PATH}"
