6e5545552895ee6d845e4df5699e38573e38c47c
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-21 23:18:50 -0700

    yes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM ubuntu
+FROM alpine:latest
+
+RUN apk add --update --no-cache bash rsync
 
 COPY --chmod=0755 entrypoint.sh /
 COPY --chmod=0755 scripts/ /scripts
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 #set -e
 
-echo "Starting: Push Artifacts"
-
 #echo "0: ${0}"
 #echo "1: ${1}"
 #echo "2: ${2}"
 
-echo "INPUT_PATH: ${INPUT_PATH}"
+echo "Starting: Push Artifacts"
 
+echo "INPUT_PATH: ${INPUT_PATH}"
 ls -lAhR "${INPUT_PATH}"
 
 echo "Running: sync.sh"
