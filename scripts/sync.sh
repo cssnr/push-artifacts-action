18452c323acd6e62388b459e0d4c8a6e56b55a92
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-22 00:04:45 -0700

    yes

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,8 +7,8 @@ echo "GITHUB_RUN_ATTEMPT: ${GITHUB_RUN_ATTEMPT}"
 
 echo "INPUT_PATH: ${INPUT_PATH}"
 echo "INPUT_HOST: ${INPUT_HOST}"
-echo "INPUT_USER: ${INPUT_USER}"
-echo "INPUT_PASS: ${INPUT_PASS}"
+#echo "INPUT_USER: ${INPUT_USER}"
+#echo "INPUT_PASS: ${INPUT_PASS}"
 echo "INPUT_PORT: ${INPUT_PORT}"
 echo "INPUT_BASE: ${INPUT_BASE}"
 
@@ -20,6 +20,6 @@ mkdir -p "${GITHUB_REPOSITORY}"
 mv "${INPUT_PATH}" "${REPO_RUN_PATH}"
 
 ls -lAhR "${REPO_RUN_PATH}"
-#sshpass -p "${INPUT_PASS}" \
-#scp -P "${INPUT_PORT}" -o StrictHostKeyChecking=no -r \
-#    "${REPO_RUN_PATH}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_BASE}/${REPO_RUN_PATH}"
+sshpass -p "${INPUT_PASS}" \
+scp -P "${INPUT_PORT}" -o StrictHostKeyChecking=no -r \
+    "${REPO_RUN_PATH}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_BASE}/${REPO_RUN_PATH}"
