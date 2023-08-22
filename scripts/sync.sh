5aaa3a017c55cbebb624c610474cd3954b5d2576
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-22 01:37:09 -0700

    rsync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -18,11 +18,35 @@ echo "REPO_RUN_PATH: ${REPO_RUN_PATH}"
 mkdir -p "${GITHUB_REPOSITORY}"
 mv "${INPUT_PATH}" "${REPO_RUN_PATH}"
 
-SOURCE=$(dirname "${GITHUB_REPOSITORY}")
-echo "SOURCE: ${SOURCE}"
+#SOURCE=$(dirname "${GITHUB_REPOSITORY}")
+echo "SOURCE: ${GITHUB_REPOSITORY_OWNER}"
 ls -lAhR "${GITHUB_REPOSITORY}"
 echo "TARGET: ${INPUT_USER}@${INPUT_HOST}:${INPUT_BASE}/${REPO_RUN_PATH}"
 
 sshpass -p "${INPUT_PASS}" \
 rsync -aPvh -e "ssh -p ${INPUT_PORT} -o StrictHostKeyChecking=no" \
-    "${SOURCE}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_BASE}"
+    "${GITHUB_REPOSITORY_OWNER}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_BASE}"
+
+if [ -n "${WEBHOOK}" ];then
+
+URL="${WEBHOST}/${REPO_RUN_PATH}"
+
+_description="""
+**${GITHUB_REPOSITORY}**
+New Artifacts Pushed =)
+[${URL}](${URL})
+"""
+
+bash discord.sh \
+  --webhook-url="${WEBHOOK}" \
+  --username "ArtifactPush" \
+  --avatar "https://i.imgur.com/12jyR5Q.png" \
+  --text "${URL}" \
+  --title "New Artifacts Pushed" \
+  --description "${_description}" \
+  --color "0xFFFFFF" \
+  --url "${URL}" \
+  --author "${GITHUB_ACTOR}" \
+  --author-url "https://github.com/${GITHUB_REPOSITORY}" \
+  --timestamp
+fi
