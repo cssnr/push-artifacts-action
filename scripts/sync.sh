9fd4d86ed5ffd0d737af3a28d5be43991476b797
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-22 20:27:22 -0700

    Debug

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+echo "GITHUB_REF_TYPE: ${GITHUB_REF_TYPE}"
+echo "GITHUB_REF: ${GITHUB_REF}"
+echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
+echo "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF}"
+echo "GITHUB_REF_NAME: ${GITHUB_REF_NAME}"
+
 echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
 echo "GITHUB_RUN_NUMBER: ${GITHUB_RUN_NUMBER}"
 echo "GITHUB_RUN_ATTEMPT: ${GITHUB_RUN_ATTEMPT}"
@@ -42,7 +48,7 @@ bash /scripts/discord.sh \
   --avatar "https://avatars.githubusercontent.com/u/84293894?s=200&v=4" \
   --text "https://github.com/${GITHUB_ACTOR}\n${URL}" \
   --title "New Artifacts Pushed" \
-  --description "**${GITHUB_REPOSITORY}**\n- New Artifacts Pushed =)\n- Served Fresh" \
+  --description "**${GITHUB_REPOSITORY}**\n- ${GITHUB_REF_NAME}\n- Served Fresh" \
   --color "0xFFFFFF" \
   --url "${URL}" \
   --author "${GITHUB_ACTOR}" \
