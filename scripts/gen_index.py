2933c947fefc54ba85fc4afd5d71eb7fa4dd31e4
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-27 00:38:27 -0700

    Fixes

--- scripts/gen_index.py
+++ scripts/gen_index.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # info_jinja = 'info.jinja'
 # init_jinja = 'init.jinja'
 
-parent_path = Path(__file__).parent.parent
+# parent_path = Path(__file__).parent.parent
 
 
 if __name__ == '__main__':
@@ -15,21 +15,20 @@ if __name__ == '__main__':
         print(f'Usage: {sys.argv[0]} [directory] [title]')
         sys.exit(1)
 
-    print(f'parent_dir: {parent_path.resolve()}')
+    print(f'title: {sys.argv[2]}')
+    # print(f'parent_path: {parent_path.resolve()}')
     target_path = Path(sys.argv[1])
     print(f'target_path: {target_path.resolve()}')
-
     print('-'*40)
     files = os.listdir(target_path)
+    files = sorted(files)
     print(files)
     print('-'*40)
-
     data = {
         'title': sys.argv[2],
         'files': files,
     }
-    env = Environment(loader=FileSystemLoader(parent_path / 'templates'))
-
+    env = Environment(loader=FileSystemLoader('/templates'))
     template = env.get_template('index.jinja2.html')
     rendered = template.render({'data': data})
     with open(target_path / f'index.html', 'w', encoding='utf-8', newline='\n') as f:
