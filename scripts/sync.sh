effe9d94da5ee6ae02fff560b33ca00459914199
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-27 00:49:04 -0700

    Fix

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -26,6 +26,7 @@ echo "REPO_RUN_PATH: ${REPO_RUN_PATH}"
 mkdir -p "${GITHUB_REPOSITORY}"
 mv "${INPUT_PATH}" "${REPO_RUN_PATH}"
 
+echo "Generating Template: index.html"
 python /scripts/gen_index.py "${REPO_RUN_PATH}" "${GITHUB_REPOSITORY}"
 
 #SOURCE=$(dirname "${GITHUB_REPOSITORY}")
