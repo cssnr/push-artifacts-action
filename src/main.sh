910687c907125ad52cf1fa311e5660ffe48c5405
Shane <6071159+smashedr@users.noreply.github.com>
2025-03-01 16:21:43 -0800

    --edit-last

--- src/main.sh
+++ src/main.sh
@@ -81,12 +81,12 @@ if [ -n "${PR_NUMBER}" ] && [ "${INPUT_COMMENT}" == "true" ];then
         if [ "${login}" == "github-actions" ];then
             body=$(echo "${comment}" | jq -r '.body')
             if [[ ${body} == Screen\ Shots\ Link:* ]]; then
-                comment_id=$(echo "${comment}" | jq -r '.id')
-                comment_url="/repos/${OWNER}/${REPO}/pulls/comments/${comment_id}"
-                echo "Editing existing comment at: ${comment_url}"
-                gh api --method PATCH -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${comment_url}" -f "body=$(cat /tmp/body)"
+                #comment_id=$(echo "${comment}" | jq -r '.id')
+                #comment_url="/repos/${OWNER}/${REPO}/pulls/comments/${comment_id}"
+                #echo "Editing existing comment at: ${comment_url}"
+                #gh api --method PATCH -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${comment_url}" -f "body=$(cat /tmp/body)"
                 #gh api --method PATCH "${comment_url}" f body="$(cat /tmp/body)"
-                #gh pr comment "${PR_NUMBER}" --body-file /tmp/body
+                gh pr comment "${PR_NUMBER}" --edit-last --body-file /tmp/body
                 commented="yes"
                 break
             fi
