a2838fa83ed76ad4b96b08733ce92b834caef792
Shane <6071159+smashedr@users.noreply.github.com>
2025-03-22 00:04:12 -0700

    Update Comment Editing

--- src/main.sh
+++ src/main.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
+
+SCRIPT_ID="<!-- cssnr/push-artifacts-action -->"
 
 echo "Running: ${0} as: $(whoami) in: $(pwd)"
 
@@ -57,7 +59,7 @@ sshpass -p "${INPUT_PASS}" \
 
 echo "----------  POST  ----------"
 
-# TODO: Add cleanup to move INPUT_SOURCE back and delete index.html
+# TODO: Add cleanup to move INPUT_SOURCE back and delete index.html | so whack
 
 URL="${INPUT_WEBHOST}/${REPO_RUN_PATH}/"
 echo "URL: ${URL}"
@@ -66,40 +68,52 @@ echo "MD_URL: ${MD_URL}"
 MD_REFS="- ${GITHUB_REF_NAME:-None}\n- ${GITHUB_HEAD_REF:-None}"
 echo -e "MD_REFS:\n${MD_REFS}"
 
+echo "Setting Outputs"
+echo "url=${URL}" >> "${GITHUB_OUTPUT}"
+
 if [[ ${GITHUB_REF} =~ refs/pull/[0-9]+/merge ]]; then
     PR_NUMBER=$(echo "${GITHUB_REF}" |  sed 's/refs\/pull\/\([0-9]\+\)\/merge/\1/')
     echo "PR_NUMBER: ${PR_NUMBER}"
 fi
 
-if [ -n "${PR_NUMBER}" ] && [ "${INPUT_COMMENT}" == "true" ];then
+if [[ -n "${PR_NUMBER}" && "${INPUT_COMMENT}" == "true" ]];then
     echo -e "\u001b[34;1mCommenting on PR: ${PR_NUMBER}"
-    echo -e "Screen Shots Link: ${MD_URL}\n${MD_REFS}" > /tmp/body
     git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-    gh pr comment "${PR_NUMBER}" --edit-last --body-file /tmp/body ||
+
+    echo -e "Screen Shots Link: ${MD_URL}\n${MD_REFS}" > /tmp/body
+
+    comments_cmd=(
+        "gh" "api" "/repos/${OWNER}/${REPO}/issues/${PR_NUMBER}/comments"
+        "-H" "Accept: application/vnd.github+json"
+        "-H" "X-GitHub-Api-Version: 2022-11-28"
+        "--jq" '.[]'
+    )
+    echo "::debug::comments_cmd: ${comments_cmd[*]}"
+    declare COMMENT
+    while read -r comment;do
+        echo "----------------------------------------"
+        echo "${comment}"
+        body=$(echo "${comment}" | jq -r '.body')
+        if [[ "${body:0:36}" == "${SCRIPT_ID}" ]]; then
+            COMMENT="${comment}"
+            break
+        fi
+    done< <( "${comments_cmd[@]}" )
+    echo "----------------------------------------"
+
+    if [[ -n "${COMMENT}" ]];then
+        comment_id=$(echo "${comment}" | jq -r '.id')
+        echo "EDIT - Existing Comment: ${comment_id}"
+        echo "original body: ${body}"
+        echo "url: /repos/${OWNER}/${REPO}/issues/comments/${comment_id}"
+        gh api --method PATCH "/repos/${OWNER}/${REPO}/issues/comments/${comment_id}" \
+            -H "Accept: application/vnd.github+json" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            -f "body=$(< /tmp/body)"
+    else
+        echo "ADD - New Comment"
         gh pr comment "${PR_NUMBER}" --body-file /tmp/body
-    #declare commented
-    #gh pr view "${PR_NUMBER}" --json comments | jq -c '.comments[]' | while read -r comment; do
-    #    login=$(echo "${comment}" | jq -r '.author.login')
-    #    echo "Checking comment by: ${login}"
-    #    if [ "${login}" == "github-actions" ];then
-    #        body=$(echo "${comment}" | jq -r '.body')
-    #        if [[ ${body} == Screen\ Shots\ Link:* ]]; then
-    #            comment_id=$(echo "${comment}" | jq -r '.id')
-    #            echo "Editing existing comment: ${comment_id}"
-    #            #comment_url="/repos/${OWNER}/${REPO}/pulls/comments/${comment_id}"
-    #            #echo "Editing existing comment at: ${comment_url}"
-    #            #gh api --method PATCH -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${comment_url}" -f "body=$(cat /tmp/body)"
-    #            #gh api --method PATCH "${comment_url}" f body="$(cat /tmp/body)"
-    #            gh pr comment "${PR_NUMBER}" --edit-last --body-file /tmp/body
-    #            commented="yes"
-    #            break
-    #        fi
-    #    fi
-    #done
-    #if [ "${commented}" != "yes" ];then
-    #    echo "Adding new comment."
-    #    gh pr comment "${PR_NUMBER}" --body-file /tmp/body
-    #fi
+    fi
 else
     echo -e "\u001b[33mSkipping comment because not PR or comment: \u001b[0m${INPUT_COMMENT}"
 fi
